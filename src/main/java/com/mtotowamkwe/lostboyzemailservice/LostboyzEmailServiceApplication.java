9d8b1c143062a1cdbfd2a96597d5dd6772225609
Michael Mkwelele <memkwelele@gmail.com>
2020-12-04 07:10:06 -0800

    Initialise repository.

---
+++ src/main/java/com/mtotowamkwe/lostboyzemailservice/LostboyzEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostboyzEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostboyzEmailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtotowamkwe/lostboyzemailservice/LostboyzEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostboyzEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
